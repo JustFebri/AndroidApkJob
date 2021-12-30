28e27de434f454d16a05fd6597f7e47ec8a73378
MikaelHans <73513335+MikaelHans@users.noreply.github.com>
2021-12-30 12:17:45 +0700

    WARD

--- KotlinMessenger/app/src/main/java/com/example/kotlinmessenger/myJobs.kt
+++ KotlinMessenger/app/src/main/java/com/example/kotlinmessenger/myJobs.kt
@@ -1,6 +1,5 @@
 package com.example.kotlinmessenger
 
-import android.app.DatePickerDialog
 import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
@@ -8,9 +7,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinmessenger.jobs.adapterJobs
@@ -18,7 +14,6 @@ import com.example.kotlinmessenger.jobs.jobItem
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,12 +22,11 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [job.newInstance] factory method to
+ * Use the [myJobs.newInstance] factory method to
  * create an instance of this fragment.
  */
-
-val listjobs = arrayListOf<jobItem>()
-class job : Fragment() {
+val myJobsList = arrayListOf<jobItem>()
+class myJobs : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -51,23 +45,24 @@ class job : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_job, container, false)
+        return inflater.inflate(R.layout.fragment_my_jobs, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         val recyclerview : RecyclerView = view.findViewById((R.id.rvJobs))
         getData(recyclerview)
-
     }
 
-    private fun getData(recyclerview:RecyclerView){
+    private fun getData(recyclerview: RecyclerView){
         val uid = FirebaseAuth.getInstance().uid
         db.collection("dbJobs")
+            .whereEqualTo("recruiterId", uid)
             .get()
             .addOnSuccessListener { result ->
-                listjobs.clear()
+                myJobsList.clear()
                 for (document in result) {
-                    listjobs.add(jobItem(document.get("id").toString(),
+                    myJobsList.add(jobItem(document.get("id").toString(),
                         document.get("title").toString(),
                         document.get("description").toString(),
                         document.get("recruiterId").toString()))
@@ -88,12 +83,12 @@ class job : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment job.
+         * @return A new instance of fragment myJobs.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            job().apply {
+            myJobs().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
